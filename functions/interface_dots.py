ff5a3a36088ee7d77c1a3d11f80d19f5aa3833f1
shiganai <shiganai.hiro@gmail.com>
2023-03-03 13:30:58 +0900

    [update & fix] interface dots: generating random dots
    also fixes num_vertical and _horizontal def

--- functions/interface_dots.py
+++ functions/interface_dots.py
@@ -1,18 +1,25 @@
-def print_dots(dots_kind):
-    print(dots_kind)
+def generate_random_dots(num_horizontal=5, num_vertical=10, num_kind=4,num_dummy_kind=1):
+    import numpy as np
+    dots_kind_matrix = np.random.randint(0, num_kind + num_dummy_kind + 1,(num_vertical, num_horizontal))
+    dots_kind_matrix = dots_kind_matrix.astype(np.float32)
+    dots_kind_matrix[dots_kind_matrix>num_kind]=np.nan
+    return dots_kind_matrix
 
-def scat_dots(dots_kind):
+def print_dots(dots_kind_matrix):
+    print(dots_kind_matrix)
+
+def scat_dots(dots_kind_matrix):
     import numpy as np
 
-    dots_kind = np.flipud(dots_kind) # flip ud so that the scatter matches print
-    dots_kind[np.isnan(dots_kind)] = -1 # replace nan to take max
+    dots_kind_matrix = np.flipud(dots_kind_matrix) # flip ud so that the scatter matches print
+    dots_kind_matrix[np.isnan(dots_kind_matrix)] = -1 # replace nan to take max
 
-    max_kinds = dots_kind.max() # get max for for-loop index
+    max_kinds = dots_kind_matrix.max() # get max for for-loop index
     max_kinds = int(max_kinds) # make the max as int for using range()
     
     # get the shape of box
-    num_horizontal = dots_kind.shape[0]
-    num_vertical = dots_kind.shape[1]
+    num_horizontal = dots_kind_matrix.shape[1]
+    num_vertical = dots_kind_matrix.shape[0]
 
     size = 172 * 1 # set scatter size. 
     #TODO: search scatter normalization, or use circle plot using radius
@@ -20,8 +27,8 @@ def scat_dots(dots_kind):
     import matplotlib.pyplot as scat
     f, ax = scat.subplots()
     for target_kind in range(max_kinds + 1):
-        target_kind_yx = np.where(dots_kind==target_kind) # find indecies of target kinds
+        target_kind_yx = np.where(dots_kind_matrix==target_kind) # find indecies of target kinds
         ax.scatter(target_kind_yx[1], target_kind_yx[0], s=size) # scatter. Be aware that the indecies are in shape of (y,x)
 
-    ax.axis([-1, num_vertical, -1, num_horizontal]) # set axis limit
+    ax.axis([-1, num_horizontal, -1, num_vertical]) # set axis limit
     ax.set_aspect(1) # normalize the length in the figure
--- sample_scripts/using_display_dots.py
+++ sample_scripts/using_display_dots.py
@@ -1,25 +1,7 @@
-import os
-
-print('getcwd:      ', os.getcwd())
-print('__file__:    ', __file__)
-
-# import modules
-import numpy as np
-import matplotlib.pyplot as scat
-
-num_horizontal = 5
-num_vertical = 10
-num_kind = 4
-num_dummy_kind = 1
-
 from functions import interface_dots
-
-dots_kind = np.random.randint(0, num_kind + num_dummy_kind + 1,(num_vertical, num_horizontal))
-dots_kind = dots_kind.astype(np.float32)
-dots_kind[dots_kind>num_kind]=np.nan
-
-interface_dots.print_dots(dots_kind)
-interface_dots.scat_dots(dots_kind)
+dots_kind_matrix = interface_dots.generate_random_dots()
+interface_dots.print_dots(dots_kind_matrix)
+interface_dots.scat_dots(dots_kind_matrix)
 
 import matplotlib.pyplot as plt
 plt.show()
