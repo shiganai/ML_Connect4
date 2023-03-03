46e6f031793d1defd6badeb15fb16d46b43a2aea
shiganai <shiganai.hiro@gmail.com>
2023-03-03 15:48:51 +0900

    [add] immediate dots falling

--- functions/interface_dots.py
+++ functions/interface_dots.py
@@ -1,4 +1,4 @@
-def generate_random_dots(num_horizontal=5, num_vertical=10, num_kind=4,num_dummy_kind=1):
+def generate_random_dots(num_horizontal=5, num_vertical=10, num_kind=3,num_dummy_kind=2):
     import numpy as np
     dots_kind_matrix = np.random.randint(0, num_kind + num_dummy_kind + 1,(num_vertical, num_horizontal))
     dots_kind_matrix = dots_kind_matrix.astype(np.float32)
@@ -12,10 +12,13 @@ def scat_dots(dots_kind_matrix):
     import numpy as np
 
     dots_kind_matrix = np.flipud(dots_kind_matrix) # flip ud so that the scatter matches print
-    dots_kind_matrix[np.isnan(dots_kind_matrix)] = -1 # replace nan to take max
+    nan_replace_matrix = np.isnan(dots_kind_matrix)
+    dots_kind_matrix[nan_replace_matrix] = -1 # replace nan to take max
 
     max_kinds = dots_kind_matrix.max() # get max for for-loop index
     max_kinds = int(max_kinds) # make the max as int for using range()
+
+    dots_kind_matrix[nan_replace_matrix] = np.nan # replace -1 back to nan
     
     # get the shape of box
     num_horizontal = dots_kind_matrix.shape[1]
@@ -32,3 +35,21 @@ def scat_dots(dots_kind_matrix):
 
     ax.axis([-1, num_horizontal, -1, num_vertical]) # set axis limit
     ax.set_aspect(1) # normalize the length in the figure
+
+def fall_dots_once(dots_kind_matrix):
+    import numpy as np
+
+    not_nan_matrix = ~np.isnan(dots_kind_matrix)
+    print(not_nan_matrix)
+    nan_sorted_indecies = np.argsort(not_nan_matrix,axis=0)
+    print(nan_sorted_indecies)
+
+    # get the shape of box
+    num_horizontal = dots_kind_matrix.shape[1]
+
+    for target_horizontal_index in range(num_horizontal):
+        target_vertical_vector = dots_kind_matrix[:,target_horizontal_index]
+        target_vertical_vector = target_vertical_vector[nan_sorted_indecies[:,target_horizontal_index]]
+        # print(target_vertical_vector)
+        dots_kind_matrix[:,target_horizontal_index] = target_vertical_vector
+    return dots_kind_matrix
---
+++ sample_scripts/animate_dots_falling.py
@@ -0,0 +1,10 @@
+from functions import interface_dots
+dots_kind_matrix = interface_dots.generate_random_dots()
+interface_dots.print_dots(dots_kind_matrix)
+interface_dots.scat_dots(dots_kind_matrix)
+
+dots_kind_matrix = interface_dots.fall_dots_once(dots_kind_matrix)
+interface_dots.scat_dots(dots_kind_matrix)
+
+import matplotlib.pyplot as plt
+plt.show()
