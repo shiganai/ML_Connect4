a3b7075cf557da8d0a90e2f3af54e3a53d98b46c
shiganai <shiganai.hiro@gmail.com>
2023-03-05 20:43:45 +0900

    [update debug] enable jupyter debug

--- sample_scripts/animate_dots_falling.py
+++ sample_scripts/animate_dots_falling.py
@@ -1,4 +1,8 @@
+import importlib
+
 from functions import interface_dots
+importlib.reload(interface_dots)
+
 dots_kind_matrix = interface_dots.generate_random_dots(num_dummy_kind=3)
 interface_dots.print_dots(dots_kind_matrix)
 
@@ -7,10 +11,17 @@ interface_dots.print_dots(dots_kind_matrix_falled)
 
 interface_dots.animate_dots_no_motion(dots_kind_matrix_falled)
 import matplotlib.pyplot as plt
-plt.show()
+from IPython import display
+plt.show(block=False)
 
 anime = interface_dots.animate_dots_no_motion([dots_kind_matrix, dots_kind_matrix_falled])
-plt.show()
+html = display.HTML(anime.to_jshtml())
+# html = display.HTML(anime.to_html5_video())
+display.display(html)
+plt.close()
 
 anime = interface_dots.animate_dots_no_motion([dots_kind_matrix, dots_kind_matrix_falled], anime_mode='artists')
-plt.show()
+html = display.HTML(anime.to_jshtml())
+# html = display.HTML(anime.to_html5_video())
+display.display(html)
+plt.close()
